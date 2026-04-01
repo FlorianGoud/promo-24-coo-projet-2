29ea66950c0272c7a03f4610fec882f60cf20d42
FlorianGoud <goudyf@3il.fr>
2026-04-01 09:55:28 +0200

    Feat : StandardDuck

--- src/duckcorp/duck/StandardDuck.java
+++ src/duckcorp/duck/StandardDuck.java
@@ -7,9 +7,23 @@ package duckcorp.duck;
  *   - Faites hériter cette classe de Duck
  *   - Implémentez le constructeur StandardDuck(int qualityScore) avec un appel à super
  *   - Implémentez getBasePrice() et describe()
- * @author Roussille Philippe <roussille@3il.fr>
+ * @author GOUDY Florian <goudyf@3il.fr>
  */
-public class StandardDuck {
+public class StandardDuck extends Duck {
 
     public static final double BASE_PRICE = 25.0;
+
+    public StandardDuck(int qualityScore) {
+        super(DuckType.STANDARD, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Canard Standard";
+    }
 }
