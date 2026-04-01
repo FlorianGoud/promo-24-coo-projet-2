15a56997e6041b58b4802b6c5ad706b5c423a2ec
FlorianGoud <goudyf@3il.fr>
2026-04-01 10:00:40 +0200

    Feat : LuxuryDuck

--- src/duckcorp/duck/LuxuryDuck.java
+++ src/duckcorp/duck/LuxuryDuck.java
@@ -7,9 +7,23 @@ package duckcorp.duck;
  *   - Faites hériter cette classe de Duck
  *   - Implémentez le constructeur LuxuryDuck(int qualityScore) avec un appel à super
  *   - Implémentez getBasePrice() et describe()
- * @author Roussille Philippe <roussille@3il.fr>
+ * @author Goudy Florian <goudyf@3il.fr>
  */
-public class LuxuryDuck {
+public class LuxuryDuck extends Duck {
 
     public static final double BASE_PRICE = 80.0;
+
+    public LuxuryDuck(int qualityScore) {
+        super(DuckType.LUXURY, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Canard de Luxe";
+    }
 }
