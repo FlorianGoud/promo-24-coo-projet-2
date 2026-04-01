b2847082a37815eab3baf942ca59f0b505951166
FlorianGoud <goudyf@3il.fr>
2026-04-01 10:55:03 +0200

    Feat : LuxuryMold

--- src/duckcorp/machine/LuxuryMold.java
+++ src/duckcorp/machine/LuxuryMold.java
@@ -1,19 +1,35 @@
 package duckcorp.machine;
 
 import duckcorp.duck.Duck;
+import duckcorp.duck.LuxuryDuck;
+import duckcorp.duck.DuckType;
 
 /**
  * Moule produisant des Canards de Luxe.
- *
- * TODO (Ex2) :
- *   - Faites hériter cette classe de Machine
- *   - Implémentez le constructeur sans paramètre avec un appel à super
- *   - Implémentez produceDuck(), getPurchaseCost(), getName()
- * @author Roussille Philippe <roussille@3il.fr>
  */
-public class LuxuryMold {
+public class LuxuryMold extends Machine {
 
     public static final int PURCHASE_COST    = 800;
     public static final int CAPACITY         = 2;
     public static final int MAINTENANCE_COST = 80;
+
+    public LuxuryMold() {
+        super(DuckType.LUXURY, CAPACITY, MAINTENANCE_COST);
+    }
+
+    @Override
+    public Duck produceDuck() {
+        int quality = computeQuality();
+        return new LuxuryDuck(quality);
+    }
+
+    @Override
+    public int getPurchaseCost() {
+        return PURCHASE_COST;
+    }
+
+    @Override
+    public String getName() {
+        return "Moule de Luxe";
+    }
 }
