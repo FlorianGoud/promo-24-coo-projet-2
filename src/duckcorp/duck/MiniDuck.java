e0c5929e99a45d0586069bb72775454053d0b9f8
FlorianGoud <goudyf@3il.fr>
2026-04-01 09:54:02 +0200

    Feat : MiniDuck

--- src/duckcorp/duck/MiniDuck.java
+++ src/duckcorp/duck/MiniDuck.java
@@ -7,9 +7,23 @@ package duckcorp.duck;
  *   - Faites hériter cette classe de Duck
  *   - Implémentez le constructeur MiniDuck(int qualityScore) avec un appel à super
  *   - Implémentez getBasePrice() et describe()
- * @author Roussille Philippe <roussille@3il.fr>
+ * @author GOUDY Florian <goudyf@3il.fr>
  */
-public class MiniDuck {
+public class MiniDuck extends Duck {
 
     public static final double BASE_PRICE = 12.0;
+
+    public MiniDuck(int qualityScore) {
+        super(DuckType.MINI, qualityScore);
+    }
+
+    @Override
+    public double getBasePrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public String describe() {
+        return "Mini Canard";
+    }
 }
